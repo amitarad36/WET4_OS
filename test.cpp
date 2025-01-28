65a7fe125b135a27a15e00ade2514560077054ef
amitarad36 <129197826+amitarad36@users.noreply.github.com>
2025-01-28 13:25:41 +0200

    testing 10.2

--- test.cpp
+++ test.cpp
@@ -4,12 +4,18 @@ extern size_t _num_meta_data_bytes();
 extern size_t _size_meta_data();
 
 int main() {
-    size_t before = _num_meta_data_bytes();
-    //void* p1 = smalloc(100);
+    size_t before = _num_meta_data_bytes();  // Ensure it's used in output
+    void* p1 = smalloc(100);
     size_t after = _num_meta_data_bytes();
 
-    if (after == before + _size_meta_data()) std::cout << "Meta Data Tracking: Passed\n";
-    else std::cout << "Meta Data Tracking: Failed\n";
+    std::cout << "Metadata Before: " << before << ", After: " << after << std::endl;
+
+    if (after == before + _size_meta_data()) {
+        std::cout << "Meta Data Tracking: Passed\n";
+    }
+    else {
+        std::cout << "Meta Data Tracking: Failed\n";
+    }
 
     return 0;
 }
