6fa13c435fb458e2ff4cd4d2b7ec127cb058d31f
amitarad36 <129197826+amitarad36@users.noreply.github.com>
2025-01-28 11:53:58 +0200

    testing 5.1

--- test.cpp
+++ test.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 extern void* smalloc(size_t size);
-extern void sfree(void* p);
 extern size_t _num_allocated_blocks();
-extern size_t _num_free_blocks();
 
 int main() {
     void* p1 = smalloc(100);
     void* p2 = smalloc(200);
-    std::cout << "Allocated Blocks: " << _num_allocated_blocks() << "\n";
 
-    sfree(p1);
-    std::cout << "Free Blocks: " << _num_free_blocks() << "\n";
+    std::cout << "Allocated Blocks: " << _num_allocated_blocks() << "\n";
+    std::cout << "Pointer p1: " << p1 << ", Pointer p2: " << p2 << std::endl;
 
     return 0;
 }
