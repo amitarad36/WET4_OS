ee6c646cfec5ae5cea1fb0876b60017c20b8f6f0
amitarad36 <129197826+amitarad36@users.noreply.github.com>
2025-01-28 11:41:18 +0200

    testing 5

--- test.cpp
+++ test.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
-#include <cstring>
 extern void* smalloc(size_t size);
-extern void* srealloc(void* oldp, size_t size);
 extern void sfree(void* p);
+extern size_t _num_allocated_blocks();
+extern size_t _num_free_blocks();
 
 int main() {
-    char* p1 = (char*)smalloc(10);
-    strcpy(p1, "Hello");
+    void* p1 = smalloc(100);
+    void* p2 = smalloc(200);
+    std::cout << "Allocated Blocks: " << _num_allocated_blocks() << "\n";
 
-    char* p2 = (char*)srealloc(p1, 20);
-
-    if (p2 && strcmp(p2, "Hello") == 0) std::cout << "srealloc: Success\n";
-    else std::cout << "srealloc: Failed\n";
+    sfree(p1);
+    std::cout << "Free Blocks: " << _num_free_blocks() << "\n";
 
     return 0;
 }
