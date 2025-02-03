a7dbc6de87569f494cb7861d328c479dfd3a7c7d
amitarad36 <129197826+amitarad36@users.noreply.github.com>
2025-02-03 20:43:25 +0200

    aaaaa2

--- allocate_memory.cpp
+++ allocate_memory.cpp
@@ -1,26 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <size>\n", argv[0]);
-        return 1;
-    }
-
-    int size = atoi(argv[1]);
-    if (size <= 0) {
-        fprintf(stderr, "Error: Size must be a positive integer\n");
-        return 1;
-    }
-
-    void* ptr = malloc(size);
-    if (!ptr) {
-        fprintf(stderr, "Error: Memory allocation failed\n");
-        return 1;
-    }
-
-    printf("Allocated %d bytes at %p\n", size, ptr);
-    free(ptr);
-
+int main(int argc, char** argv) {
+    unsigned int x = atoi(argv[1]);
+    void* memory = malloc(x);
+    free(memory);
     return 0;
 }
